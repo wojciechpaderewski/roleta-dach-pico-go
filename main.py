1e1e2e63115b0bf269a356cd7592500bb3732567
Wojciech <wojtpaderewski@gmail.com>
2023-07-26 15:26:00 +0200

    working manual version

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import HAcomunication
+# import HAcomunication
 import control
 import time
 
 
-print('Starting main.py')
-initziled = HAcomunication.init()
+print('Starting main.py xd')
+# initziled = HAcomunication.init()
 
 while True:
-    if initziled:
-        HAcomunication.update()
+    # if initziled:
+    #     # HAcomunication.update()
         control.updateManualControl()
